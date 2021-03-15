fe5a7baf2c5e77eeab99ecca015235351662f935
sovietdarknight <80577697+sovietdarknight@users.noreply.github.com>
2021-03-16 00:22:23 +0300

    Update mars.js

--- mars.js
+++ mars.js
@@ -1,1 +1,23 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://spam-1-20.glitch.me/`);
+}, 280000);
+
+//
+//
+const Discord = require("discord.js");
+const Canvas = require("canvas");
+const fs = require("fs");
+const cmd = require("node-cmd");
+
+/////// const by mars for token///////
+
+const MARS = new Discord.Client();
+const MARS2 = new Discord.Client();
 
